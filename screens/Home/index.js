fa23c191192363e3a3496fcbd300e9dcbf3ff883
Shahin Dostuyev <108731857+ShahinDostuyev@users.noreply.github.com>
2024-06-07 18:36:32 +0300

    Bug fixed

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -79,13 +79,13 @@ function HomeScreen() {
           </View>
           <View style={styles.potInfo}>
             <Text style={styles.timeInfoText}>
-              {nextWateringTime.days && "0"} days
+              { nextWateringTime ? nextWateringTime.days : "0"} days
             </Text>
             <Text style={styles.timeInfoText}>
-              {nextWateringTime.hours && "0"} hours
+              {nextWateringTime ? nextWateringTime.hours : "0"} hours
             </Text>
             <Text style={styles.timeInfoText}>
-              {nextWateringTime.minutes && "0"} minutes
+              {nextWateringTime ? nextWateringTime.minutes : "0"} minutes
             </Text>
             <Text style={styles.infoTitle}>Next watering</Text>
           </View>
